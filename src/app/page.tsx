5d12ec8e749773743afe5346bf298fc041821d6b
tientcheudonald237 <103892120+tientcheudonald237@users.noreply.github.com>
2025-01-08 03:30:18 +0100

    Integration complet jusqu'as la gestion des profils

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
-import { prisma } from "@/lib/prisma";
+import { getPosts } from "@/actions/post.action";
+import { getDbUserId } from "@/actions/user.action";
+import CreatePost from "@/components/CreatePost";
+import PostCard from "@/components/PostCard";
+import WhoToFollow from "@/components/WhoToFollow";
+import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
-  // await prisma.p
+  const user = await currentUser();
+  const posts = await getPosts();
+  const dbUserId = await getDbUserId();
+
   return (
-    <div className='m-4'>
-      <h1>Home page</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
+
+        <div className="space-y-6">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+          ))}
+        </div>
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <WhoToFollow />
+      </div>
     </div>
   );
 }
